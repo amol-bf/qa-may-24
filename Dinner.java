b46af20e3bbf42e8e5834c36b3cd25789fc7a347
amol-bf <amolkathale2703@gmail.com>
2025-05-24 14:59:07 +0530

    Add files via upload
    final commit

---
+++ Dinner.java
@@ -0,0 +1,23 @@
+package test;
+
+public class Dinner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int Dinner=1000;
+		int breakfast=2000;
+		int carRental=3000;
+		  
+		//int mealExpense=3000;
+		//int totalexpense=6000;
+		
+		
+		int mealExpense=Dinner+breakfast;
+		int totalexpense=mealExpense+carRental;
+		  System.out.println(" MealExpense = " + mealExpense + ";");
+	      System.out.println(" TotalExpense = " + totalexpense + ";");
+
+	}
+
+}
