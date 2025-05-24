4aa1d45be43e02e1e9c320c918ac74b8bfa1182c
amol-bf <amolkathale2703@gmail.com>
2025-05-24 15:16:24 +0530

    Update Dinner.java

--- Dinner.java
+++ Dinner.java
@@ -1,23 +1,49 @@
 package test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Dinner {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		int Dinner=1000;
-		int breakfast=2000;
-		int carRental=3000;
-		  
-		//int mealExpense=3000;
-		//int totalexpense=6000;
-		
-		
-		int mealExpense=Dinner+breakfast;
-		int totalexpense=mealExpense+carRental;
-		  System.out.println(" MealExpense = " + mealExpense + ";");
-	      System.out.println(" TotalExpense = " + totalexpense + ";");
-
+	Map<String, Integer> expensesType;
+	
+	public Dinner(){
+		expensesType = new HashMap<>();
 	}
+	 public void addExpense(String type, int amount) {
+		 expensesType.put(type, amount);
+	    }
+	 
+	 public int getMealExpense() {
+	        int mealExpense = 0;
+	        if (expensesType.containsKey("dinner")) {
+	            mealExpense += expensesType.get("dinner");
+	        }
+	        if (expensesType.containsKey("breakfast")) {
+	            mealExpense += expensesType.get("breakfast");
+	        }
+	        return mealExpense;
+	    }
+
+	    // Calculate total expense
+	    public int getTotalExpense() {
+	        int total = 0;
+	        for (int amount : expensesType.values()) {
+	            total += amount;
+	        }
+	        return total;
+	    }
+	    public static void main(String[] args) {
+	    	Dinner obj = new Dinner();
+	        
+	        // Adding expenses
+	    	obj.addExpense("Dinner", 1000);
+	    	obj.addExpense("breakfast", 2000);
+	    	obj.addExpense("car rental", 3000);
+
+	        // Output
+	        System.out.println("meal expense = " + obj.getMealExpense());
+	        System.out.println("Total Expense = " + obj.getTotalExpense());
+	    }
 
 }
